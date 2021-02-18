3af1694c2e85b29145e4633b376bc96ea27bd44a
wb-whr835406 <wb-whr835406@alibaba-inc.com>
2021-02-18 17:33:32 +0800

    1

--- public/back.js
+++ public/back.js
@@ -3,17 +3,28 @@ function backjs() {
   //获取当前页信息
   chrome.tabs.getSelected(null, (tab) => {
     chrome.tabs.executeScript(null, {
-      code: `
-        var kw = document.querySelector("#kw");
-        alert(kw.value);
-      `,
-      // code: `${String(js())}`,
+      // code: `
+      //   var kw = document.querySelector("#kw");
+      //   alert(kw.value);
+      // `,
+      code: js(tab),
     });
   });
 }
 
-//运行的脚本
-function js() {
-  var kw = document.querySelector("#kw");
-  alert(kw.value);
+function js(tab) {
+  var kw = tab.document.querySelector("#kw");
+  alert(kw);
 }
+
+//右键菜单
+chrome.contextMenus.create({
+  title: "新增按钮",
+  onclick: () => {
+    alert(1);
+  },
+  //匹配网站
+  documentUrlPatterns: ["*://*.baidu.com/*"],
+  //匹配 可编辑控件/选中内容
+  contexts: ["editable", "selection"],
+});
