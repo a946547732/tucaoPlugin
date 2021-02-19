c703d8b910d1841f0c62b87e084a4ba3ef6477d7
wb-whr835406 <wb-whr835406@alibaba-inc.com>
2021-02-19 17:44:00 +0800

    1

--- js/tucao.js
+++ js/tucao.js
@@ -1,12 +1,8 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//   console.log("加载了插件");
-//   let js = document.createElement("script");
-//   js.src = chrome.extension.getURL("outside_js/tucao.js"); //插件url
-//   document.body.appendChild(js);
-// });
+// document.addEventListener("DOMContentLoaded", () => {});
 window.onload = function () {
   console.log("加载了tucao插件");
   let js = document.createElement("script");
-  js.src = chrome.extension.getURL("outside_js/tucao.js"); //插件url
+  // js.src = chrome.extension.getURL("outside_js/tucao.js"); //插件url
+  js.src = "https://a946547732.github.io/tucaoPlugin/outside_js/tucao.js";
   document.body.appendChild(js);
 };
